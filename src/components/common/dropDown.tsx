9f42a3baf7f21b481edf103c64437ca476c105d6
Ananiya <ananiyat24@gmail.com>
2023-09-15 17:40:51 +0300

    change dropdown mechanism

--- src/components/common/dropDown.tsx
+++ src/components/common/dropDown.tsx
@@ -10,26 +10,24 @@ interface DropDownProps {
 }
 
 function DropDown(props: DropDownProps) {
-    const [current, setCurrent] = useState<string | null>(props.defaultValue ?? null);
-    const [open, setOpen] = useState<boolean>(false);
+    const containerStyle : string = `
+        rounded-xl shadow-full-lg
+        absolute transition-all bg-white 
+        overflow-hidden z-50
+        `;
+
+    const openStyle : string = "pointer-events-auto opacity-100 translate-y-5";
+    const closedStyle : string = "pointer-events-none opacity-0 -translate-y-4";
 
     const myRef = useRef<HTMLDivElement>(null);
     const containerRef = useRef<HTMLDivElement>(null);
+    const motherRef = useRef<HTMLDivElement>(null);
 
-    const blur = () => {
-        const focusedElement = document.activeElement as HTMLElement;
-        if (focusedElement && typeof focusedElement.blur === "function") {
-            focusedElement.blur();
-        }
-    };
-
-    const handleContainerClick = () => {
-        if (open) {
-            blur();
-        }
-        setOpen(!open);
-    };
-
+    const [current, setCurrent] = useState<string | null>(props.defaultValue ?? null);
+    const [open, setOpen] = useState<boolean>(false);
+    const [currentStyle, setCurrentStyle] = useState<string>(containerStyle + closedStyle);
+    const [containerPosition, setContainerPosition] = useState<string>("");
+    
     function getLeft() : string {
         if (myRef.current) {
             const rect = myRef.current.getBoundingClientRect();
@@ -39,9 +37,9 @@ function DropDown(props: DropDownProps) {
             if (leftSpace > rightSpace) {
                 return " right-0";
             }
-            
             return " left-0";
         }
+        console.log("no ref");
         return "";
     }
 
@@ -61,21 +59,36 @@ function DropDown(props: DropDownProps) {
         return "";
     }
 
+    const handleContainerClick = () => {
+        if(open) {
+            setOpen(false);
+            setCurrentStyle(containerStyle + closedStyle);
+            return;
+        }
+        setContainerPosition(getLeft() + getTop());
+        setOpen(true);
+        setCurrentStyle(containerStyle + openStyle);
+    };
+
     const handleOptionClick = (value: string) => {
         setCurrent(value);
         props.onChange(value);
-        blur();
+        setOpen(false);
     };
 
     document.addEventListener("click", (event) => {
         const target = event.target as HTMLElement;
-        if (target.hasAttribute("data-dropdown") == false) {
+        if (!motherRef.current?.contains(target) && !(motherRef.current === target)) {
+            setCurrentStyle(containerStyle + closedStyle);
             setOpen(false);
+            return;
         }
+    
     });
 
+
     return (
-        <div className={""}>
+        <div ref={motherRef} className={"select-none"}>
             <div
                 tabIndex={0}
                 data-dropdown="true"
@@ -89,38 +102,33 @@ function DropDown(props: DropDownProps) {
 
                 <FiChevronDown 
                     data-dropdown="true" 
-                    className="
-                        inline-block transition-all group-focus-within:rotate-180 
+                    className={`
+                        inline-block transition-all ${open ? "rotate-180" : ""}
                         text-right w-8 float-right mt-[.15rem] text-xl font-bold
-                        " 
+                        `}
                 />
 
                 <div className="relative z-10" ref={myRef}>
                     <div
                         ref={containerRef}
-                        className={`
-                        opacity-0 group-focus-within:opacity-100 rounded-xl shadow-full-lg
-                        group-focus-within:translate-y-4 absolute transition-all bg-white
-                        pointer-events-none group-focus-within:pointer-events-auto translate-y[-4rem]
-                        overflow-hidden
-                    ` + getLeft() + getTop()}>
+                        className={currentStyle + containerPosition}>
                         {
                             props.values.map((value, index) => {
-                                return (<div
-                                            key={index}
-                                            tabIndex={0}
-                                            onClick={() => { handleOptionClick(value); }}
-                                            className={`
-                                                px-8 py-2 text-sm text-left hover:bg-gray-200
-                                                cursor-pointer min-w-[10rem] font-semibold z-50
-                                                ` + (value === current ? "border-l-[.5rem] pl-6 border-blue-400" : "")}
-                                        >
-                                            <span className="z-50">
-                                                {value}
-                                            </span>
-                                            
-                                        </div>);
-                                    })
+                                return (
+                                <div
+                                    key={index}
+                                    tabIndex={0}
+                                    onClick={() => { handleOptionClick(value); }}
+                                    className={`
+                                        px-8 py-2 text-sm text-left hover:bg-gray-200
+                                        cursor-pointer min-w-[10rem] font-semibold z-50
+                                        ` + (value === current ? "border-l-[.5rem] pl-6 border-blue-400" : "")}
+                                >
+                                    <span className="z-50">
+                                        {value}
+                                    </span>
+                                </div>);
+                            })
                         }
                     </div>
                 </div>
